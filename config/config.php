a4555bddb66817cb18fe5b046a9cbebd52b894b8
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-03-19 11:43:58 +0100

    Commit : -Création du fichier config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'db_name'   => 'mg_parents',
+    'db_user'   => 'root',
+    'db_pass'   => '',
+    'db_host'   => 'localhost',
+
+    'directory' => '/cvtek/view/'
+);
