ff21412a66a20316e5cca764a4d47cd3b7f770dd
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-03-19 12:53:35 +0100

    Commit : -header avec route fait

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -12,10 +12,9 @@
     <header>
       <nav>
           <ul>
-              <li><a href="<?= $view->path('home'); ?>">Hoome</a></li>
-              <li><a href="<?= $view->path('contact'); ?>">Contact</a></li>
-              <li><a href="<?= $view->path('single',array(12)); ?>">Single</a></li>
-              <li><a href="<?= $view->path('single2',array(12,'dedede')); ?>">Single2</a></li>
+              <li><a href="<?= $view->path('home'); ?>">Home</a></li>
+              <li><a href="<?= $view->path('inscription'); ?>">Inscription</a></li>
+              <li><a href="<?= $view->path('connexion'); ?>">Connexion</a></li>
           </ul>
       </nav>
     </header>
