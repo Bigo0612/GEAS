91d22395da72ba094abac31b8daa4e3f3996a59d
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-03-20 14:17:15 +0100

    Commit : -debut de la home fini

--- view/app/default/frontpage.php
+++ view/app/default/frontpage.php
@@ -3,20 +3,18 @@
 <h2>Un endroit pour <span>garder vos enfants ?</span></h2>
 
 <div class="garde">
-    <img src="img/bebe.jpg" alt="Image de deux bébés">
+    <div class="flex2"><img src="../public/asset/img/bebe.png" alt="Image d'un bébé"></div>
     <div class="gardeEnfant"><a href="#">Garder mes enfants</a></div>
 </div>
 
 <div class="info">
-    <div class="flex1">
-        <h3>Besoin d'information pour <br><span>devenir un mini garde ?</span></h3>
-    </div>
+    <div class="flex1"><h3>Besoin d'information pour <br><span>devenir un mini garde ?</span></h3></div>
     <div class="gouv"><a href="#">info-gouv</a></div>
 </div>
 
 <div class="gardien">
     <div class="gardeEnfant"><a href="#">Je garde des enfants</a></div>
-    <img src="img/creche.svg" alt="Image de crèche">
+    <img src="./asset/img/creche.svg" alt="Image de crèche">
 </div>
 
 </section>
