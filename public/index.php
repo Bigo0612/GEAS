ea5cb8c9e1b16de84b8dbed4cb016808ee9cd711
Bigo0612 <quesada.steve@gmail.com>
2020-03-23 23:35:41 +0100

    initialisation index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 date_default_timezone_set('Europe/Paris');
 session_start();
 
+if(file_exists('../vendor/autoload.php')) {
+    require('../vendor/autoload.php');
+}
 // Autoloader (namespace)
 require('../app/Autoloader.php');
 \App\Autoloader::register();
@@ -9,5 +12,5 @@ require('../app/Autoloader.php');
 $app = \App\App::getInstance();
 // Routes
 require('../config/routes.php');
-//$router = new \App\Weblitzer\Router($routes);
+$router = new \App\Weblitzer\Router($routes);
 
