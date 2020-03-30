4d94a11f834754cfef028164fb0d32c673e9804f
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-03-30 15:45:03 +0200

    Fin de la home page

--- view/app/default/frontpage.php
+++ view/app/default/frontpage.php
@@ -1,6 +1,7 @@
 <section id="homePage">
 
 <h2>Un endroit pour <span>garder vos enfants ?</span></h2>
+<p class="acroche"><span>MiniGarde</span> est un site ou vous pouvez trouver facilement un endroit<span> pour garder vos enfants</span> selons votre position. L'objectif est de vous aider dans la <span>garde de votre enfant.</span> Grace à MiniGarde, chercher un gardien en quelques clics</p>
 
 <div class="garde">
     <div class="flex2"><img src="../public/asset/img/bebe.png" alt="Image d'un bébé"></div>
