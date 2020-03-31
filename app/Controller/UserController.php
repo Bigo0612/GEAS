5d092b569435eb2dceda4b8420b738a8efef9e9d
Bigo0612 <quesada.steve@gmail.com>
2020-03-31 14:31:09 +0200

    public function connexion public function motDePasseOublie public function modifMotDePasse

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -8,11 +8,12 @@ use App\Model\UserModel;
 use App\Service\Form;
 use App\Service\Validation;
 
+
 class UserController extends Controller
 {
+
     public function register()
     {
-        $title = '';
 
         $errors = array();
         $form = new Form($errors, 'post');
@@ -33,10 +34,88 @@ class UserController extends Controller
         }
 
         $this->render('app.default.register', array(
-            'title' => $title,
             'form' => $form,
             'errors' => $errors
         ));
+    }
+
+    public function connexion()
+    {
+
+        $errors = array();
+        $form = new Form($errors, 'post');
+        if (isset($_POST['submitted'])) {
+            $post = $this->cleanXss($_POST);
+            $valid = new Validation();
+            $errors['mail'] = $valid->emailValid($post['mail']);
+
+            if ($valid->IsValid($errors) == true) {
+                $user = UserModel::userLogin($post['mail']);
+                if ($user->email === $post['mail'] && password_verify($post['password'], $user->pass)) {
+                    $_SESSION = array(
+                        'id'    => $user->id,
+                        'nom'   => $user->nom,
+                        'prenom'=> $user->prenom,
+                        'role'  => $user->role,
+                        'email' => $user->email,
+                        'ip'    => $_SERVER['REMOTE_ADDR']
+                    );
+                    header('Location: index.php?page=home');
+                } else {
+                    $errors['password'] = 'Mot de passe ou mail incorrect';
+                }
+            } else {
+                $errors['connexion'] = 'Erreur dans les identifiants';
+            }
+        } else {
+            $errors['email'] = 'Erreur dans le fomulaire';
+        }
+
+        $this->render('app.default.connexion', array(
+            'form'  => $form,
+            'errors' => $errors
+        ));
+    }
+
+    public function motDePasseOublie()
+    {
+        $title = 'Mot de passe oublié';
+        $html = '';
+        if (!empty($_POST['submitted'])) {
+            $post = $this->cleanXss($_POST);
+            $check = UserModel::userLogin($post['email']);
+            $token = $check->token;
+            if (!empty($check && !empty($token))) {
+                $html = '<a href="index.php?page=modifMotDePasse&token='.$token.'">Changer de mot de passe</a>';
+            } else {
+                $html = 'Email inconnu';
+            }
+        }
+
+        $this->render('app.default.motDePasseOublie', compact('title', 'html'));
+    }
+
+    public function modifMotDePasse()
+    {
+        $title = 'Changer votre mot de passe';
+        $valid = new Validation();
+        $um = new UserModel();
+        $i = $um->checkId($_GET['token']);
+        $um->setId($i->id);
+        $id = $um->getId();
+        if (!empty($id)) {
+            if (!empty($_POST['submitted'])) {
+                $post = $this->cleanXss($_POST);
+                $password = $valid->textValid($post['password1'], 'mot de passe', 5, 20);
+                $hash = password_hash($password, PASSWORD_BCRYPT);
+                $token = UserModel::generateToken(255);
+                UserModel::changePassword($hash, $token, $id);
+            }
+        } else {
+            echo 'no';
+        }
+
+        $this->render('app.default.modifMotDePasse', compact('title'));
     }
 
 
