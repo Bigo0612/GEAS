88fb6ba005b6f98070192faa72a643b327e6c306
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-04-19 04:30:32 +0200

    d

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,42 +1,31 @@
 function geoFindMe() {
-  const status = document.querySelector("#status");
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
 
   function error() {
-    status.textContent = "Impossible de recevoir votre géolocalisation";
+    status.textContent = 'Unable to retrieve your location';
   }
 
   if (!navigator.geolocation) {
-    status.textContent =
-      "La geolocalisation n'est pas supporter sur votre naviguateur";
+    status.textContent = 'Geolocation is not supported by your browser';
   } else {
-    status.textContent = "Locating…";
+    status.textContent = 'Locating…';
     navigator.geolocation.getCurrentPosition(success, error);
   }
+
 }
 
-function insertParam(key, value)
-{
-    key = encodeURI(key); value = encodeURI(value);
-
-    var kvp = document.location.search.substr(1).split('&');
-
-    var i=kvp.length; var x; while(i--) 
-    {
-        x = kvp[i].split('=');
-
-        
-        if (x[0]==key)
-        {
-            x[1] = value;
-            kvp[i] = x.join('=');
-            break;
-        }
-    }
-
-    if(i<0) {kvp[kvp.length] = [key,value].join('=');}
-
-    //this will reload the page, it's likely better to store this until finished
-    document.location.search = kvp.join('&'); 
-}
-
-document.querySelector('#find').addEventListener('click', insertParam);
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
