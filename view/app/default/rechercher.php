3dbe169d4672e383e33e47533ffee1e72dcd63ff
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-04-19 05:35:51 +0200

    d

--- view/app/default/rechercher.php
+++ view/app/default/rechercher.php
@@ -1,52 +1,52 @@
-<button id = "find-me">Montrer ma localisation</button><br/>
-<p id = "status"></p>
-<a id = "map-link" target="_blank"></a>
+<button id="find-me">Montrer ma localisation</button><br />
+<p id="status"></p>
+<div><a id="map-link"></a></div>
 
 <script>
-function geoFindMe() {
+    function geoFindMe() {
 
-const status = document.querySelector('#status');
-const mapLink = document.querySelector('#map-link');
+        const status = document.querySelector('#status');
+        const mapLink = document.querySelector('#map-link');
 
-mapLink.href = '';
-mapLink.textContent = '';
+        mapLink.href = '';
+        mapLink.textContent = '';
 
-function success(position) {
-  const latitude  = position.coords.latitude;
-  const longitude = position.coords.longitude;
-
-  
+        function success(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
 
 
-  var url = new URL('http://localhost/GEAS/public/rechercher');
-var search_params = url.searchParams;
 
-// add "longitude" parameter
-search_params.set(`longitude`,  `${longitude}` );
-search_params.set(`latitude`,  `${latitude}` );
 
-url.search = search_params.toString();
+            var url = new URL('http://localhost/GEAS/public/rechercher');
+            var search_params = url.searchParams;
 
-var new_url = url.toString();
+            // add "longitude" parameter
+            search_params.set(`longitude`, `${longitude}`);
+            search_params.set(`latitude`, `${latitude}`);
 
-status.textContent = '';
-  mapLink.href = new_url;
-  mapLink.textContent = 'Trouver les crèches';
+            url.search = search_params.toString();
 
-}
+            var new_url = url.toString();
 
-function error() {
-  status.textContent = 'Unable to retrieve your location';
-}
+            status.textContent = '';
+            mapLink.href = new_url;
+            mapLink.textContent = 'Trouver les crèches';
 
-if (!navigator.geolocation) {
-  status.textContent = 'Geolocation is not supported by your browser';
-} else {
-  status.textContent = 'Locating…';
-  navigator.geolocation.getCurrentPosition(success, error);
-}
+        }
 
-}
+        function error() {
+            status.textContent = 'Unable to retrieve your location';
+        }
 
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
+        if (!navigator.geolocation) {
+            status.textContent = 'Geolocation is not supported by your browser';
+        } else {
+            status.textContent = 'Locating…';
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+
+    }
+
+    document.querySelector('#find-me').addEventListener('click', geoFindMe);
 </script>
